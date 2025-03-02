4e5aed93eca58761a9f423b694f21bdd78a52bbe
pseudo03 <127375569+pseudo03@users.noreply.github.com>
2025-03-02 20:21:51 +0000

    commit 3

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let num = "555";
+
+// console.log(num);
+// console.log(typeof num);
+
+// let convert = Number(num);
+
+// console.log(typeof (convert));
+
+// console.log(convert);
+
+///////////////////////////////OPERATIONS/////////////////////////////////
+
+let str1 = "vedant"
+let str2 = " rotto"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(3+4+"5");
+
